36738149bac133aa394c5819d289754a8c617768
William McKee <williamcmckee@protonmail.com>
2024-03-18 14:20:07 +1300

    add sense hat endpoint

--- api/index.py
+++ api/index.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+from sense_hat import SenseHat
+
+sense = SenseHat()
 
 app = FastAPI()
 
+@app.get("/api/humidity")
+def humidity_data():
+    
+    return {"value": str(sense.get_humidity())}
+
+@app.get("/api/env")
+def temp_data():
+    return {"humidity" : str(sense.get_humidity()),
+            "temperature": str(sense.get_temperature()),
+            "tempfrmhum" : str(sense.get_temperature_from_humidity()),
+            "tempfrmpressure" : str(sense.get_temperature_from_pressure()),
+            "pressure" : str(sense.get_pressure())}
+
+@app.get("/api/tempfrmhumidity")
+def temphum_data():
+    return {"value" : str(sense.get_temperature_from_humidity())}
+
+@app.get("/api/tempfrmpressure")
+def temppre_data():
+    return {"value" : str(sense.get_temperature_from_pressure())}
+
+@app.get("/api/pressure")
+def pressure_data():
+    return {"value" : str(sense.get_temperature_from_pressure())}
+
+
 @app.get("/api/python")
 def hello_world():
-    return {"message": "Hello World"}
+	return {"Hello" : "World"}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+
+	const [data, setData] = useState(null)
+        const [isLoading, setLoading] = useState(true)
+
+        useEffect(() => {
+                fetch('/api/env')
+                .then((res) => res.json())
+                .then((data) => {
+                        setData(data)
+                        setLoading(false)
+                })
+        }, [])
+        if (isLoading) return <p>Loading...</p>
+        if (!data) return <p>No env data</p>
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
-          <Link href="/api/python">
-            <code className="font-mono font-bold">api/index.py</code>
+          <Link href="/api/env">
+            <code className="font-mono font-bold">{ data }</code>
           </Link>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
